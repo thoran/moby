f7cc41fc9a2f2f18c2fa06cb0275e44b9f538ad6
thoran <code@thoran.com>
2024-06-05 23:51:05 +1000

    Committed automatically by git-importall.

--- lib/Whale.rb
+++ lib/Whale.rb
@@ -1,80 +1,75 @@
+# Whale.rb
 # Whale
-
-# 20110907
-
-# Description: Sometimes when they go fishing, they get a whale and it sinks their boat.  
-
-# Changes since 0.5: 
-# 1. 
-
-# Todo: 
-# 1. Command line options library needed really, since fixed order options is a bit blerrr.  Done as of 0.4.4, but really working as of 0.4.5.  
-# 2. Be able to set the username and password fields by number.  Done as of 0.4.7.  
-# 3. On-the-fly randomly generated passwords.  
-# 4. Randomly generate different real user-agent strings.  
-# 5. Use open proxies and/or tor at random, since any logging will identify many submissions from the same IP address and with different logins.  
-
-# Roadmap:
-# 0.5.x will have automated hooks into dslreports.com/phishtrack.  
-# 0.6.x will have a generalised interface into hooking in phishing databases and will have one or two additional databases.  
-# 0.7.x will offer threading for maximum throughput!  
-# 0.8.x will optionally randomize submission frequency, such that a maximum and a minimum number of submissions can be set per period.  
 
 require 'pp'
 
-require 'rubygems'
 require 'mechanize'
 
-require 'Array/extract_optionsX'
 require 'File/self.collect'
-require 'Switches'
-
-$debug = false
-
-TLD = %w{com net org edu int mil gov arpa biz aero name coop info pro museum}
-TLD_SIZE = 15
 
 class Whale
-  
-  VERSION = '0.6.0'
-  
-  attr_accessor :options
-  attr_accessor :url
-  attr_accessor :form_name, :form_number
-  attr_accessor :username_field_name, :username_field_number
-  attr_accessor :password_field_name, :password_field_number
-  attr_accessor :verbose, :username_is_email_address, :user_agent
-  attr_reader :words_size
-  
-  def initialize(url = nil, *args)
-    options = args.extract_options!
-    @url = options[:url]
-    @form_name = options[:f] || options[:form] || options[:form_name]
-    @form_number = options[:n] || options[:form_number] || 0
-    @username_field_name = options[:user_field] || options[:username_field] || options[:user_field_name] || options[:username_field_name] || options[:username_fieldname] || 'username'
-    @username_field_number = options[:user_field_number] || options[:username_field_number]
-    @password_field_name = options[:pass_field] || options[:password_field] || options[:pass_field_name] || options[:password_field_name] || 'password'
-    @password_field_number = options[:pass_field_number] || options[:password_field_number]
-    @verbose = options[:v] || options[:verbose] || false
-    @username_is_email_address = options[:e] || options[:username_is_email_address] || false
-    @user_agent = options[:a] || options[:user_agent] || 'Windows IE 6'
-    @words_size = words.size
+
+  TLD = %w{com net org edu int mil gov arpa biz aero name coop info pro museum}
+
+  attr_accessor\
+    :debug,
+    :form_name,
+    :form_number,
+    :password_field_name,
+    :password_field_number,
+    :url,
+    :user_agent,
+    :username_field_name,
+    :username_field_number,
+    :username_hostname,
+    :username_is_email_address,
+    :verbose
+
+  def initialize(
+    debug: false,
+    form_name: nil,
+    form_number: 0,
+    password_field_name: 'password',
+    password_field_number: 1,
+    url: nil,
+    user_agent: nil,
+    username_field_name: 'username',
+    username_field_number: 0,
+    username_hostname: nil,
+    username_is_email_address: false,
+    verbose: false
+  )
+    @debug = debug
+    @form_name = form_name
+    @form_number = form_number
+    @password_field_name = password_field_name
+    @password_field_number = password_field_number
+    @url = url
+    @user_agent = user_agent
+    @username_field_name = username_field_name
+    @username_field_number = username_field_number
+    @username_hostname = username_hostname
+    @username_is_email_address = username_is_email_address
+    @verbose = verbose
   end
-  
+
+  def mechanize
+    @mechanize ||= Mechanize.new
+  end
+
   def counter_phish
     start_time = Time.now
     puts "Whale session begun #{start_time}."
     submission_count = 0
-    @m = Mechanize.new
-    @m.user_agent_alias = user_agent
     begin
       loop do
+        set_user_agent
         set_username_field
         set_password_field
-        @m.submit(form)
-        pp @m.submit(form) if $debug
+        result = mechanize.submit(form)
+        pp result if @debug
         submission_count += 1
-        puts "#{submission_count} #{username}:#{password}" if verbose
+        puts "#{submission_count} #{username}:#{password}" if @verbose
       end
     ensure
       finish_time = Time.now
@@ -82,37 +77,44 @@ class Whale
       submissions_per_minute = submission_count / time_delta_in_minutes
       puts "Whale session terminated #{finish_time} with #{submission_count} counter-phishes served in #{time_delta_in_minutes} minutes for an average of #{submissions_per_minute} submissions per minute."
     end
-  end # def counter_phish
-  
+  end
+
   private
-  
+
   def words(filename = '/usr/share/dict/words')
     @words ||= File.collect(filename){|line| line.chomp}
   end
-  
+
   def random_word
-    words[rand(words_size)]
+    words[rand(words.size)]
   end
-  
+
   def random_TLD
-    TLD[rand(TLD_SIZE)]
+    TLD[rand(TLD.size)]
   end
-  
-  def page # This is a big timesaver.  I think I don't need to keep grabbing the page everytime.  
+
+  def random_user_agent
+    agent_aliases = Mechanize::AGENT_ALIASES
+    agent_aliases.delete('Mechanize')
+    agent_aliases_keys = agent_aliases.keys
+    agent_aliases_keys[rand(agent_aliases.size)]
+  end
+
+  def page
     @page ||= (
-      _page = @m.get(@url)
+      _page = mechanize.get(@url)
       md = _page.body.match(/meta.*?Refresh.*?url=(.*?)"/i)
       if md
-        puts 'meta_refresh_url found' if $debug
-        _page = @m.get(md[1])
-        pp _page if $debug
+        puts 'meta_refresh_url found' if @debug
+        _page = mechanize.get(md[1])
+        pp _page if @debug
       end
-      pp _page if $debug
+      pp _page if @debug
       _page
     )
   end
-  
-  def form # Similarly to #page.  I don't think I need to grab the form everytime either.  
+
+  def form
     @form ||= (
       if form_name
         page.form(form_name)
@@ -123,19 +125,31 @@ class Whale
       end
     )
   end
-  
+
   def username
     if username_is_email_address
-      "#{random_word}@#{random_word}.#{random_TLD}"
+      if username_hostname
+        "#{random_word}@#{username_hostname}"
+      else
+        "#{random_word}@#{random_word}.#{random_TLD}"
+      end
     else
       random_word
     end
   end
-  
+
   def password
     random_word
   end
-  
+
+  def set_user_agent
+    if @user_agent
+      mechanize.user_agent_alias = @user_agent
+    else
+      mechanize.user_agent_alias = random_user_agent
+    end
+  end
+
   def set_username_field
     if username_field_name
       form[username_field_name] = username
@@ -145,7 +159,7 @@ class Whale
       form.fields[0] = username
     end
   end
-  
+
   def set_password_field
     if password_field_name
       form[password_field_name] = password
@@ -155,5 +169,5 @@ class Whale
       form.fields[1] = password
     end
   end
-  
+
 end
